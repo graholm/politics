eaef1f200f751e03b930e90882aa6fe1c63151dc
graholm <graholm@nuevaschool.org>
2016-12-06 08:54:29 -0800

    Classadoo autocommit

--- script.js
+++ script.js
@@ -2,15 +2,17 @@ $.ajax({
   method: "GET",
   url: "https://www.govtrack.us/api/v2/person",
   success: function(data) {  
-    
+    var arr = []
+  
+    arr.push(object.firstname)
+    console.log (object.firstname) })
+  
     data.objects.forEach(function(object) {
       console.log(object)
     })
-  }
-})
 
-var arr = []
 
-data.object.forEach = (function(data){
-  arr.push(object.firstname)
-console.log (object.firstname) })
+
+
+
+ 
